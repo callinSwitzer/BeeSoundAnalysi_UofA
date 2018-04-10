28d70dbb5b2ff16a681e29f612b3713cdb804681
callinSwitzer <callin.switzer@gmail.com>
2018-04-10 12:57:01 -0700

    made fig3 on linear scale

--- SoundAnalysisForSteve.R
+++ SoundAnalysisForSteve.R
@@ -134,3 +134,71 @@ mtext('C', at = c(51, 15), cex = 2 * 2/3)
 dev.off()
 
 
+
+tiff("~/Dropbox/dataAnalysisForOthers/SoundAnalysisforSteve/Fig3_no_dB.tiff",width = 6.5, height = 8, units = 'in', res = 1200)
+{
+     aa = cutw(w2, from = 1, to = 2, f = w2@samp.rate)
+     layout(matrix(c(1,1,2), nrow = 3, ncol = 1, byrow = TRUE))
+     par(mai = c(0,0,0,0))
+     plot(y = aa / max(aa), x = seq(0, 1, length.out = length(aa)), type = 'l', bty = "n", ylim = c(-4.5, 1), 
+          yaxt = "n", xlab = "", ylab = "", ann = FALSE, xaxt = "n"
+     )
+     # add scale
+     arrows(x0 = 0.8, y0 = -1.1, x1 = 1, y1 = -1.1, lwd = 1.2, length = 0.05, code = 3)
+     text(0.9, -1.15, "0.2 s", adj = c(0.5, 1.3), cex = 1.1)
+     
+     # add line segments
+     x0 = (1.435 - 1) 
+     x1 = (1.515 - 1) 
+     y0 = -0.3
+     y1 = -0.9
+     segments(x0, y0, x1 = x0, y1, lwd = 1.2)
+     segments(x0 = x1, y0, x1 = x1, y1, lwd = 1.2)
+     
+     # add arrows
+     arrows(x0, y0 = y1, x1 = 0, y1 = y1 - 1, lwd = 1.2, length = 0.05)
+     arrows(x0 = x1, y0 = y1, x1 = 1, y1 = y1 - 1, lwd = 1.2, length = 0.05)
+     
+     # add next layer
+     bb = cutw(w2, from = x0 + 1, to = x1 + 1, f = w2@samp.rate)
+     lines(bb/ max(bb) - 3,  x = seq(0, 1, length.out = length(bb)))
+     
+     # add scale
+     arrows(x0 = 0.8, y0 = -4.5+0.3, x1 = 1, y1 = -4.5 + 0.3, lwd = 1.2, length = 0.05, code = 3)
+     text(0.9, -4.55+0.3, "0.014 s", adj = c(0.5, 1.3), cex = 1.1)
+     
+     # add letters
+     text('A', x= 0, y = 1, cex = 2)
+     text('B', x= 0, y = -1.6, cex = 2)
+     
+     
+     # plot spectrum
+     spp <- spec((bb/max(bb)), f = w2@samp.rate, scaled = TRUE, fftw = TRUE, PSD = FALSE, wl = 1024, main = "FFT spectrum of zoomed section", plot = FALSE, norm = FALSE)
+     par(mai = c(0.6,0.7,0,0))
+     
+     spectrum = data.frame(unlist(spp))
+     xx <- c(spectrum$x * 1000, rev(spectrum$x * 1000))
+     ref = max(spectrum$y)
+     # calculate dB, with 0 being the max
+     dB = spectrum$y/max(spectrum$y)
+     yy <- c(rep(min(dB), length(dB)), rev(dB))
+     
+     yy = yy[xx >= 99]
+     xx = xx[xx>=99]
+     
+     plot(xx, yy, type = 'l', ylab = "Relative amplitude", xlab = "Frequency (Hz)", 
+          bty = "n", log = "x", cex.lab=1.1, xlim = c(100, 22050), yaxt="n")
+     axis(side = 2, at = c(0,1))
+     polygon(x = xx, y = yy, col='black', border=NA)
+     
+     # add text
+     text(round(xx[yy == max(yy)]), y = 1, labels = paste(round(xx[yy == max(yy)]), "Hz", sep  = " "), adj = c(-0.2, 0.5), cex = 1.1)
+     
+     mtext('C', at = c(51, 15), cex = 2 * 2/3)
+     
+     
+     
+}
+dev.off()
+
+
